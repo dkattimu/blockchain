a20a2e81a9ab1e2f4231d00d25ba499bf8b75ee3
D A <dkattimu@gmail.com>
2022-01-12 13:11:08 -0500

    first commit

---
+++ Multi-Send.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.7.0 <0.9.0;
+//<SPDX-License-Identifier:none
+contract MyGame {
+    uint public playerCount = 0;
+   
+    enum Level {Novice, Intermediate, Advanced}
+
+    struct Player{
+        address playerAddress;
+        Level playerLevel;
+        string firstName;
+        string lastName;
+        uint createdTime;
+    }
+    mapping (address => Player) public players;
+
+    function addPlayer(string memory firstName, string memory lastName) public returns (Player) {
+        players[msg.sender] = Player(msg.sender, Level.Novice, firstName, lastName, block.timestamp);
+        return (Player) players[msg.sender];
+    }
+
+    function getPlayerLevel(address playerAddress) public view returns (Level) {
+        Player storage player = players[playerAddress];
+        return player.playerLevel;
+    }
+
+    function changePlayerLevel(address playerAddress) public {
+        Player storage player = players[playerAddress];
+        if (block.timestamp >= player.createdTime + 20) {
+            player.playerLevel = Level.Intermediate;
+        }
+    }
+}
